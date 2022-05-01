b3e7db20b198a1c36be942a3ae6787ef7c40d6b5
samuelfer <samuelfesant@gmail.com>
2022-05-01 10:59:31 -0300

    Add service Create Product

---
+++ src/modules/products/services/CreateProductService.ts
@@ -0,0 +1,31 @@
+import { ProductRepository } from '../typeorm/repositories/ProductsRespository';
+import { getCustomRepository } from 'typeorm';
+import AppError from '@shared/errors/AppError';
+import Product from '../typeorm/entities/Product';
+
+interface IRequest {
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+class CreateProductService {
+
+  public async execute({ name, price, quantity }: IRequest): Promise<Product> {
+    const productRepository = getCustomRepository(ProductRepository);
+    const productExists = await productRepository.findByName(name);
+
+    if (productExists) {
+      throw new AppError('There is already one product with this name');
+    }
+
+    const product = productRepository.create({
+      name, price, quantity,
+    });
+
+    await productRepository.save(product);
+    return product;
+  }
+}
+
+export default CreateProductService;
