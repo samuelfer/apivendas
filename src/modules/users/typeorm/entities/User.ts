a229dacbeccaca27689114efa4c07a7b5120731e
samuelfer <samuelfesant@gmail.com>
2022-05-01 15:38:57 -0300

    Add entity Users

--- src/modules/users/typeorm/entities/User.ts
+++ src/modules/users/typeorm/entities/User.ts
@@ -4,22 +4,24 @@ import {
   Entity,
   PrimaryGeneratedColumn,
   UpdateDateColumn
-} from 'typeorm';
+} from "typeorm";
 
-@Entity('products')
-class Product {
-
+@Entity('users')
+class User {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @Column()
   name: string;
 
-  @Column('decimal')
-  price: number;
+  @Column()
+  email: string;
 
-  @Column('int')
-  quantity: number;
+  @Column()
+  password: string;
+
+  @Column()
+  avatar: string;
 
   @CreateDateColumn()
   created_at: Date;
@@ -28,4 +30,4 @@ class Product {
   updated_at: Date
 }
 
-export default Product;
+export default User;
