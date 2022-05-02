c53c3e169873ac85dd355c7f039cb696d8c096a5
samuelfer <samuelfesant@gmail.com>
2022-05-02 20:50:43 -0300

    Add controller e routes para users

--- src/shared/http/routes/index.js
+++ src/shared/http/routes/index.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import productRouter from '@modules/products/routes/product.routes';
+import userRouter from '@modules/users/routes/user.routes';
 
 const routes = Router();
 
 routes.use('/products', productRouter);
+routes.use('/users', userRouter);
 
 export default routes;
