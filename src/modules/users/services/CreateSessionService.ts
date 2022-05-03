fe08305a62a4e09f1bde5312d77bb20f95d51d2a
samuelfer <samuelfesant@gmail.com>
2022-05-02 21:37:35 -0300

    Gera token para user com a biblioteca jsonwebtoken

--- src/modules/users/services/CreateSessionService.ts
+++ src/modules/users/services/CreateSessionService.ts
@@ -1,17 +1,24 @@
-import { getCustomRepository } from 'typeorm';
 import AppError from '@shared/errors/AppError';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import { getCustomRepository } from 'typeorm';
+
 import User from '../typeorm/entities/User';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
-import { compare, hash } from 'bcryptjs';
 
 interface IRequest {
   email: string;
   password: string;
 }
 
+interface IResponse {
+  user: User;
+  token: string;
+}
+
 class CreateSessionService {
 
-  public async execute({ email, password }: IRequest): Promise<User> {
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
     const usersRepository = getCustomRepository(UsersRepository);
     const user = await usersRepository.findByEmail(email);
 
@@ -25,7 +32,15 @@ class CreateSessionService {
       throw new AppError('Invalid email/password', 401);
     }
 
-    return user;
+    const token = sign({}, '67615bfb7068d55c929dde291d3226e8', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return {
+      user,
+      token,
+    };
   }
 }
 
