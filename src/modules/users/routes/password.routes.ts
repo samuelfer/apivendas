e1b2ddc610532a03e6839b2e272779b4ce42b515
samuelfer <samuelfesant@gmail.com>
2022-05-09 21:41:47 -0300

    Add rout para Redefinir senha

--- src/modules/users/routes/password.routes.ts
+++ src/modules/users/routes/password.routes.ts
@@ -1,11 +1,12 @@
 import { celebrate, Joi, Segments } from 'celebrate';
 import { Router } from 'express';
-import ForgotPasswordController from '../controllers/ForgotPasswordController';
 
-import SessionController from '../controllers/SessionController';
+import ForgotPasswordController from '../controllers/ForgotPasswordController';
+import ResetPasswordController from '../controllers/ResetPasswordController';
 
 const passwordRouter = Router();
 const forgotPasswordController = new ForgotPasswordController();
+const resetPasswordController = new ResetPasswordController();
 
 passwordRouter.post('/forgot',
   celebrate({
@@ -14,6 +15,17 @@ passwordRouter.post('/forgot',
     },
   }),
   forgotPasswordController.create
+);
+
+passwordRouter.post('/reset',
+  celebrate({
+    [Segments.BODY]: {
+      token: Joi.string().uuid().required(),
+      password: Joi.string().required(),
+      password_confirmation: Joi.string().required().valid(Joi.ref('password')),
+    },
+  }),
+  resetPasswordController.create
 );
 
 export default passwordRouter;
