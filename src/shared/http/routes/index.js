3ef7f638273b1bab0594c8fc6b80143256c3231d
samuelfer <samuelfesant@gmail.com>
2022-05-09 21:21:55 -0300

    Add route para forgot password

--- src/shared/http/routes/index.js
+++ src/shared/http/routes/index.js
@@ -1,6 +1,8 @@
 import productRouter from '@modules/products/routes/product.routes';
+import passwordRouter from '@modules/users/routes/password.routes';
 import sessionRouter from '@modules/users/routes/session.routes';
 import userRouter from '@modules/users/routes/user.routes';
+
 import { Router } from 'express';
 
 const routes = Router();
@@ -8,5 +10,6 @@ const routes = Router();
 routes.use('/products', productRouter);
 routes.use('/users', userRouter);
 routes.use('/sessions', sessionRouter);
+routes.use('/password', passwordRouter);
 
 export default routes;
