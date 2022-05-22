78b116478cd3993fff4fd40b1db0524135399c08
samuelfer <samuelfesant@gmail.com>
2022-05-22 15:50:01 -0300

    Add routes de perfil de usuário

--- src/shared/http/routes/index.js
+++ src/shared/http/routes/index.js
@@ -1,5 +1,6 @@
 import productRouter from '@modules/products/routes/product.routes';
 import passwordRouter from '@modules/users/routes/password.routes';
+import profileRouter from '@modules/users/routes/profile.routes';
 import sessionRouter from '@modules/users/routes/session.routes';
 import userRouter from '@modules/users/routes/user.routes';
 
@@ -11,5 +12,6 @@ routes.use('/products', productRouter);
 routes.use('/users', userRouter);
 routes.use('/sessions', sessionRouter);
 routes.use('/password', passwordRouter);
+routes.use('/profile', profileRouter);
 
 export default routes;
