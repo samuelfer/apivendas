09f07f4c49e316412c8df82f798d3f88b26757d9
samuelfer <samuelfesant@gmail.com>
2022-05-22 16:35:52 -0300

    Import no indes de routes a class de routes de clientes

--- src/shared/http/routes/index.js
+++ src/shared/http/routes/index.js
@@ -1,3 +1,4 @@
+import customerRouter from '@modules/customers/routes/customer.routes';
 import productRouter from '@modules/products/routes/product.routes';
 import passwordRouter from '@modules/users/routes/password.routes';
 import profileRouter from '@modules/users/routes/profile.routes';
@@ -13,5 +14,6 @@ routes.use('/users', userRouter);
 routes.use('/sessions', sessionRouter);
 routes.use('/password', passwordRouter);
 routes.use('/profile', profileRouter);
+routes.use('/customers', customerRouter);
 
 export default routes;
